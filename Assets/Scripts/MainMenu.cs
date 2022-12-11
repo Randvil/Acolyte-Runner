e7f4b75dd83c20b4a6366d482b6b8d6cfe8a5a23
AnCap-60 <assasin149maks@gmail.com>
2022-12-11 15:37:15 +0300

    Часть меню сделана
    Звуки надо понять...

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play() => SceneManager.LoadScene(1);
+
+    public void Quit() => Application.Quit();
+
+    public void Music()
+    {
+
+    }
+
+    public void Sound()
+    {
+
+    }
+}
